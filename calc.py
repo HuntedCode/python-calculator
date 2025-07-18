3ab6b6627ab4584138d3842cd2892aaa9fed86a6
Jeffrey Lowe <HuntedCode47@gmail.com>
2025-07-18 12:16:05 -0400

    Add divde by 0 handler and clarifying statements

--- calc.py
+++ calc.py
@@ -6,17 +6,27 @@ while(True):
     if(num.isnumeric()):
         num = float(num)
         break
+    else:
+        print("That is not a valid number!")
     
 while(True):
     operator = input("Please enter an operator (+, -, *, /, %, ^): ")
     if operator in accepted_operators:
         break
+    else:
+        print("That is not a valid operator!")
 
 while(True):
     num2 = input("Please enter a number: ")
     if(num2.isnumeric()):
         num2 = float(num2)
+
+        if (operator == "/" and num2 == 0):
+            print("Cannot divide by 0!")
+            continue
         break
+    else:
+        print("That is not a valid number!")
 
 match operator:
     case "+":
