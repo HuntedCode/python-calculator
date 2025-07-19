6cc5d46ebaba2aedb006e99dced84e7d296682f3
Jeffrey Lowe <HuntedCode47@gmail.com>
2025-07-19 11:34:25 -0400

    Add multi-calculation functionality and docstrings.

--- calc.py
+++ calc.py
@@ -1,17 +1,23 @@
 num, num2, total = 0, 0, 0
-accepted_operators = {"+", "-", "*", "/", "%", "^"}
+accepted_operators = {"+", "-", "*", "/", "%", "^", ""}
 
 def get_input_number():
+    """Returns valid numerical input from user."""
+
     while(True):
-        num = input("Please enter a number: ")
-        if num.isnumeric():
-           return float(num)
+        try:
+            user_input = float(input("Please enter a number: "))
+        except ValueError:
+           print("That is not a valid number!")
         else:
-            print("That is not a valid number!")
+            return user_input
+            
 
 def get_input_operator():
+    """Returns valid mathematical operator from user."""
+
     while(True):
-        operator = input("Please enter an operator (+, -, *, /, %, ^): ")
+        operator = input("Please enter an operator (+, -, *, /, %, ^) or leave it blank to finish your calculation: ")
         if operator in accepted_operators:
             break
         else:
@@ -20,12 +26,16 @@ def get_input_operator():
     return operator
 
 def check_divide_by_zero(operator, operand):
+    """Checks if user is dividng by 0 and returns True if detected."""
+
     if (operator == "/" and operand == 0):
-        return False
-    else:
         return True
+    else:
+        return False
 
 def perform_calculation(current_total, operator, operand):
+    """Performs mathematical calculation based on number and operator params."""
+    
     new_total = 0
 
     match operator:
@@ -45,15 +55,23 @@ def perform_calculation(current_total, operator, operand):
     return new_total
 
 total = get_input_number()
-operator = get_input_operator()
 
 while(True):
-    num = get_input_number()
-    if (check_divide_by_zero(operator, num)):
+    operator = get_input_operator()
+    if operator == "":
         break
-    else:
-        print("Cannot divide by 0!")
+    
+    input_num = 0
 
-total = perform_calculation(total, operator, num)
+    while(True):
+        input_num = get_input_number()
+        if (check_divide_by_zero(operator, input_num)):
+            print("Cannot divide by 0!")
+        else:
+            break
 
-print("Total: " + str(total))
+    total = perform_calculation(total, operator, input_num)
+
+    print("Total: " + str(total))
+
+print("Final total: ", str(total))
